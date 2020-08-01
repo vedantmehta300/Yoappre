b90e1f4fff3ab10e1e3cfcc64a621b1b620b01f8
Vedant Mehta <vedantmehta300@gmail.com>
2020-08-01 19:08:15 +0530

    Updates

---
+++ precache-manifest.a16a35fdae75f3b5effba6f282ab811e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3af06c61c19c0500de3ad0a6e84bdbb1",
+    "url": "/Yoappre/index.html"
+  },
+  {
+    "revision": "afb62a11a9a87ec1aae7",
+    "url": "/Yoappre/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "54d1c93d5beb8a3f30ab",
+    "url": "/Yoappre/static/css/main.1b2a3c77.chunk.css"
+  },
+  {
+    "revision": "afb62a11a9a87ec1aae7",
+    "url": "/Yoappre/static/js/2.e830befe.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Yoappre/static/js/2.e830befe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54d1c93d5beb8a3f30ab",
+    "url": "/Yoappre/static/js/main.8d1de42c.chunk.js"
+  },
+  {
+    "revision": "e3a95d28560aa52cd630",
+    "url": "/Yoappre/static/js/runtime-main.c8ea7d22.js"
+  },
+  {
+    "revision": "a91d3c271ab420776f16d06c52109edd",
+    "url": "/Yoappre/static/media/image.a91d3c27.jpg"
+  }
+]);
